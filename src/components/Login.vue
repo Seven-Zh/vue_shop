265e3ef9bc2963f585cbdd0ef8d99c4bf99728ee
zhaozhiqi <zhaozhiqi1996@163.com>
2020-07-01 17:28:41 +0800

    登录功能完成

---
+++ src/components/Login.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录区域 -->
+      <el-form
+        label-width="0px"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRull"
+        ref="loginFormRef"
+      >
+        <el-form-item prop="name">
+          <el-input prefix-icon="el-icon-user-solid" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input prefix-icon="el-icon-lock" v-model="loginForm.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "admin",
+        password: "123456"
+      },
+      // 表单验证
+      loginFormRull: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur"
+          },
+          {
+            min: 3,
+            max: 16,
+            message: "用户名长度需要在3-16位之间",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          },
+          {
+            min: 6,
+            max: 15,
+            message: "用户名长度需要在6-15位之间",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    // 重置表单
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+    },
+    // 预验证
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        // 判断是否验证成功
+        if (!valid) return;
+        // 验证成功后发送ajax请求
+        let { data: res } = await this.$http.post("login", this.loginForm);
+        // 判断是否登录成功
+        if (res.meta.status !== 200) return this.$message.error("用户不存在");
+        this.$message.success("登录成功");
+        // 登录成功后，将token保存在sessionStorage中
+        console.log(res);
+        window.sessionStorage.setItem("token", res.data.token);
+        // 通过编程式导航跳转到home页面
+        this.$router.push("/home");
+      });
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  transform: translate(-50%, -50%);
+}
+
+.avatar_box {
+  position: absolute;
+  left: 50%;
+  width: 130px;
+  height: 130px;
+  background-color: #fff;
+  padding: 10px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  box-shadow: 0 0 10px #ddd;
+  transform: translate(-50%, -50%);
+  img {
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
